88956ad296d3bbc4930fc1b1aabcf21f228e741b
Brian Kirkpatrick <code@tythos.net>
2022-08-13 16:53:22 -0700

    temporary test

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
  * @author "Brian Kirkpatrick"
  */
 
-if (typeof(define) === "undefined") { define = (definer) => { return definer(require, exports, module); }; }
+if (typeof(define) === "undefined") { define = (factory) => { return factory(require, exports, module); }; }
 define(function(require, exports, module) {
+    const jtx = require("./lib/jtx/");
+
+    function main() {
+        console.log(jtx.Date.format(new Date(), "%Y-%m-%d"));
+    }
+
     return Object.assign(exports, {
+        "__main__": main,
         "__url__": "",
         "__semver__": "v0.1.0",
         "__license__": "",
