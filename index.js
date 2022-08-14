cf749a11bae3634bb97182bbf9a91ed3aa179dfb
Brian Kirkpatrick <code@tythos.net>
2022-08-13 18:28:02 -0700

    how did it not catch the main? ><

--- index.js
+++ index.js
@@ -2,20 +2,24 @@
  * @author "Brian Kirkpatrick"
  */
 
-if (typeof(define) === "undefined") { define = (factory) => { return factory(require, exports, module); }; }
-define(function(require, exports, module) {
-    const jtx = require("./lib/jtx/");
+if (typeof(define) !== "function" || define.amd !== true) {
+	// specify a commonjs-compatible define() factory hook
+	define = (factory) => module.exports = factory(require, {}, module);
+}
+define(function(require, _, module) {
+    const jtx = require("./lib/jtx");
 
     function main() {
         console.log(jtx.Date.format(new Date(), "%Y-%m-%d"));
     }
 
-    return Object.assign(exports, {
+    return Object.assign({}, {
         "__main__": main,
-        "__url__": "",
+        "__url__": "https://github.com/Tythos/yarnbox2.git",
         "__semver__": "v0.1.1",
         "__license__": "",
         "__deps__": {},
         "__tests__": []
     });
 });
+if (module.id === "." && module.exports.__main__) { module.exports.__main__(); }
